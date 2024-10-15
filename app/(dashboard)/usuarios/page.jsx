56d54f827d371d9640f2ef264c55287f1c5717b9
granjerolucas <56519538+granjerolucas@users.noreply.github.com>
2024-10-15 12:40:20 -0500

    labels

--- app/(dashboard)/usuarios/page.jsx
+++ app/(dashboard)/usuarios/page.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import Table from '@/components/Table'
+import React from "react";
+import Table from "@/components/Table";
 export default function page() {
-    const usuarios = [
-        { nombreCompleto: 'Juan Pérez', correo: 'juan@example.com', telefono: '123-456-7890', rol: 'Admin' },
-        { nombreCompleto: 'Ana García', correo: 'ana@example.com', telefono: '234-567-8901', rol: 'Usuario' },
-        { nombreCompleto: 'Carlos Ruiz', correo: 'carlos@example.com', telefono: '345-678-9012', rol: 'Editor' }
-      ];
-    
-      return (
-        <div className="flex justify-center items-center min-h-screen bg-gray-100">
-          <Table usuarios={usuarios} />
-        </div>
-  )
+  const usuarios = [
+    {
+      nombreCompleto: "Juan Pérez",
+      correo: "juan@example.com",
+      telefono: "123-456-7890",
+      rol: "Admin",
+    },
+    {
+      nombreCompleto: "Ana García",
+      correo: "ana@example.com",
+      telefono: "234-567-8901",
+      rol: "Usuario",
+    },
+    {
+      nombreCompleto: "Carlos Ruiz",
+      correo: "carlos@example.com",
+      telefono: "345-678-9012",
+      rol: "Editor",
+    },
+  ];
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <Table usuarios={usuarios} />
+    </div>
+  );
 }
