d4132bfe0f9811132e47373b090d42922c2312b7
granjerolucas <56519538+granjerolucas@users.noreply.github.com>
2024-10-15 10:53:16 -0500

    layout

---
+++ app/(dashboard)/home/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem commodi
+        quas eum? Ullam eaque vero quidem inventore tempore ipsum. Unde, eos
+        ipsum ullam totam iste doloribus beatae quos pariatur ratione!
+      </div>
+    </div>
+  );
+}
+
+export default page;
